73e6a6eda6893788bb95bfd13ade9ebd8d731151
lorenzo <l.neveux@icloud.com>
2024-05-01 20:31:49 +0200

    refacto requires

--- index.php
+++ index.php
@@ -1,6 +1,4 @@
 <?php
-    require('./src/tasks/read.php');
-    require('./src/views/partials/header.phtml');
-    require('./src/views/index.phtml');
-    require('./src/views/partials/footer.phtml');
+
+require('./src/tasks/read.php');
 
--- src/tasks/read.php
+++ src/tasks/read.php
@@ -3,3 +3,7 @@
 require(__DIR__ . '/../lib/csv.php');
 
 $data = readCSV('todolist');
+
+require('./src/views/partials/header.phtml');
+require('./src/views/index.phtml');
+require('./src/views/partials/footer.phtml');
